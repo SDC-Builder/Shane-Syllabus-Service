11ffdb26508c3979f3b63bccc697f967f4ba584c
Shane Sutro <shanesutro@gmail.com>
2021-05-03 21:38:46 -0600

    Add test for post route, working on the rest

--- __test__/crud.test.js
+++ __test__/crud.test.js
@@ -1,9 +1,47 @@
-const app = require('../server/app');
+/* eslint-disable no-undef */
 const request = require('supertest');
+const app = require('../server/app');
+const { SyllabusModel, connect, disconnect } = require('./db.mocks.js');
+const fixtures = require('./fixtures');
+
+beforeAll(async () => {
+  connect();
+});
+
+afterEach(async () => {
+  await SyllabusModel.deleteMany();
+});
+
+afterAll(async () => {
+  disconnect();
+});
 
 describe('Test the root path', () => {
   test('It should respond to the GET method', async () => {
     const response = await request(app).get('/');
     expect(response.statusCode).toBe(200);
+  });
+
+  test('It should allow the insertion of a document', async () => {
+    let records = await SyllabusModel.find();
+    expect(records.length).toBe(0);
+    const response = await request(app).post('/api/syllabus', { body: fixtures.sampleSyllabusString });
+    expect(response.statusCode).toBe(201);
+    records = await SyllabusModel.find();
+    expect(records.length).toBe(1);
+  });
+
+  test('It should allow modifiction of a document', async () => {
+    let records = await SyllabusModel.find();
+    expect(records.length).toBe(0);
+    await SyllabusModel.create(fixtures.sampleSyllabus);
+    const sample = fixtures.sampleSyllabus;
+    sample.weeks[0].weekNumber = 2;
+    sample.weeks[0].hoursToCompleteCourse = 800;
+    const response = await request(app).put('/api/syllabus').send({ body: JSON.stringify(sample) });
+    expect(response.statusCode).toBe(202);
+    records = await SyllabusModel.find();
+    expect(records[0].weeks[0].weekNumber).toBe(2);
+    expect(records[0].weeks[0].hoursToCompleteCourse).toBe(800);
   });
 });
--- __test__/db.mocks.js
+++ __test__/db.mocks.js
@@ -1,2 +1,19 @@
 const mongoose = require('mongoose');
+const syllabusesSchema = require('../db/data/syllabusesSchema');
 
+const connect = () => new Promise((resolve, reject) => {
+  mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology: true });
+  resolve();
+});
+
+const disconnect = () => new Promise((resolve, reject) => {
+  mongoose.disconnect();
+});
+
+const SyllabusModel = mongoose.model('syllabuses', syllabusesSchema);
+
+module.exports = {
+  SyllabusModel,
+  connect,
+  disconnect,
+};
