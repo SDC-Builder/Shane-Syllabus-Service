8340b0b01d29b74f9193a430c19112e759d1148c
Shane Sutro <shanesutro@gmail.com>
2021-05-05 21:19:51 -0600

    Add data generation files

--- db/index.js
+++ db/index.js
@@ -1,30 +1,30 @@
 const { SyllabusModel } = require('./data/syllabusesModel');
 
-const hoursToComplete = (courseNumber, cb) => {
-  SyllabusModel.findOne({ id: courseNumber })
-    .then((syllabusData) => {
-      const { hoursToCompleteCourse } = syllabusData.toObject();
-      cb({ hoursToCompleteCourse });
-    })
-    .catch((err) => {
-      if (err) {
-        console.error(err);
-      }
-    });
-};
+// const hoursToComplete = (courseNumber, cb) => {
+//   SyllabusModel.findOne({ id: courseNumber })
+//     .then((syllabusData) => {
+//       const { hoursToCompleteCourse } = syllabusData.toObject();
+//       cb({ hoursToCompleteCourse });
+//     })
+//     .catch((err) => {
+//       if (err) {
+//         console.error(err);
+//       }
+//     });
+// };
 
-const syllabus = (courseNumber, cb) => {
-  const options = { id: courseNumber };
-  SyllabusModel.findOne(options)
-    .then((syllabusData) => {
-      cb(syllabusData);
-    })
-    .catch((err) => {
-      if (err) {
-        console.error(err);
-      }
-    });
-};
+// const syllabus = (courseNumber, cb) => {
+//   const options = { id: courseNumber };
+//   SyllabusModel.findOne(options)
+//     .then((syllabusData) => {
+//       cb(syllabusData);
+//     })
+//     .catch((err) => {
+//       if (err) {
+//         console.error(err);
+//       }
+//     });
+// };
 
 const rest = {
   get: (courseNumber, cb) => {
@@ -55,7 +55,7 @@ const rest = {
 };
 
 module.exports = {
-  hoursToComplete,
-  syllabus,
+  // hoursToComplete,
+  // syllabus,
   rest,
 };
