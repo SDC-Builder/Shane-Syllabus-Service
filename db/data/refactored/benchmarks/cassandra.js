3cff969b4d6eadea03fda7b80d5007dbe64a40e8
Shane Sutro <shanesutro@gmail.com>
2021-05-10 13:20:40 -0600

    Added cassandra scripts and benchmarks

---
+++ db/data/refactored/benchmarks/cassandra.js
@@ -0,0 +1,30 @@
+const cassandra = require('cassandra-driver');
+
+const client = new cassandra.Client({
+  keyspace: 'syllabus_data',
+  contactPoints: ['127.0.0.1'],
+  localDataCenter: 'datacenter1',
+});
+
+(async () => {
+  console.time('All Queries');
+  console.time('Test One');
+  let query = 'SELECT * FROM syllabus WHERE id=9999999';
+  let result = await client.execute(query);
+  let text = JSON.parse(result.rows[0].syllabus);
+  console.timeEnd('Test One');
+
+  console.time('Test Two');
+  query = 'SELECT * FROM syllabus WHERE id=10000000';
+  result = await client.execute(query);
+  text = JSON.parse(result.rows[0].syllabus);
+  console.timeEnd('Test Two');
+
+  console.time('Test Three');
+  query = 'SELECT * FROM syllabus WHERE id=8999999';
+  result = await client.execute(query);
+  text = JSON.parse(result.rows[0].syllabus);
+  console.timeEnd('Test Three');
+  console.timeEnd('All Queries');
+  await client.shutdown();
+})();
--- db/data/refactored/dataToCsv.js
+++ db/data/refactored/dataToCsv.js
@@ -1,18 +1,20 @@
 const fs = require('fs');
 const csvWriter = require('csv-write-stream');
 const { generate } = require('./generatorNew');
+const { insert } = require('./cassandraInserter');
 
 const writer = csvWriter({ sendHeaders: false });
-writer.pipe(fs.createWriteStream('./test.csv'));
+writer.pipe(fs.createWriteStream('./test.txt'));
 
 (async () => {
   console.time('Start');
-  for (let id = 1; id <= 10; id++) {
+  for (let id = 1; id <= 10000000; id++) {
     if (id % 500000 === 0) {
       console.log('ID', id);
     }
-    writer.write({ record: generate(id) });
-
+    const record = generate(id);
+    // writer.write({ syllabus: `${record}\n` });
+    insert(id, record);
     try {
       // eslint-disable-next-line no-await-in-loop
       await new Promise((resolve) => setImmediate(resolve));
@@ -20,6 +22,6 @@ writer.pipe(fs.createWriteStream('./test.csv'));
       console.log(err);
     }
   }
-  writer.end();
+  // writer.end();
   console.timeEnd('Start');
 })();
